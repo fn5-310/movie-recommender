c72ddda023ecea114b4c4c11669d5f72d60bb846
Crynner <calvin.shih19@gmail.com>
2026-08-14 23:13:10 +1200

    refactor randInt function for min and max bounds. getRandomMovie now only returns a single movie object

--- server/controllers/movies.controller.js
+++ server/controllers/movies.controller.js
@@ -1,24 +1,27 @@
 
-const randRange = 1000
+const randRange = 500
 const randomUrl = 'https://api.themoviedb.org/3/discover/movie?page=1&vote_average.gte=0';
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`
-  }
-};
+/**
+ * 
+ * @param {int} min The minimum returned integer
+ * @param {int} max The maximum returned integer
+ * @returns A random value between min and max
+ */
+const randInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 /**
  * Selects a random page given randRange for the TMDB url
  * @returns A populated url with a random number for the page field
  */
 const randomPage = () => {
-    console.log(process.env.TMDB_ACCESS_TOKEN)
-    const page = Math.floor(Math.random() * randRange + 1)
-    const newUrl = randomUrl.replace('page=1', `page=${page}`)
-    return newUrl
+    return `https://api.themoviedb.org/3/discover/movie?page=${randInt(1, randRange)}&vote_average.gte=0`
+}
+
+const randomResult = (json) => {
+    return json.results[randInt(0,20)];
 }
 
 export const getRandomMovie = async (_req, res) => {
@@ -30,6 +33,6 @@ export const getRandomMovie = async (_req, res) => {
         }
     })
         .then(apiRes => apiRes.json())
-        .then(data => res.json(data))
+        .then(data => res.json(randomResult(data)))
         .catch(err => res.status(500).json({ message: err.message }))
 }
