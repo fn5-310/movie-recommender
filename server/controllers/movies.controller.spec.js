00216517ae82447d3853ec8dfe3c5f3327cc710e
Crynner <calvin.shih19@gmail.com>
2026-08-17 18:55:13 +1200

    moved some unit tests to integration side, and additional route tests

--- server/controllers/movies.controller.spec.js
+++ server/controllers/movies.controller.spec.js
@@ -14,26 +14,6 @@ describe("getRandomMovie", () => {
         vi.restoreAllMocks();
     });
 
-    it("gets and returns json data", async () => {
-        // if fetches, mocks json response with result
-        vi.spyOn(globalThis, "fetch").mockResolvedValue({
-            ok: true,
-            json: vi.fn().mockResolvedValue({
-                results: [{ name: "this" }, { name: "that" }],
-            }),
-            }
-        );
-
-        const res = {
-        json: vi.fn(),
-        status: vi.fn().mockReturnThis(),
-        };
-
-        await getRandomMovie({}, res);
-
-        expect(res.json).toHaveBeenCalled();
-    });
-
     it("returns correct indexed data", async () => {
         vi.mocked(randomInt).mockReturnValue(0);
 
@@ -54,20 +34,4 @@ describe("getRandomMovie", () => {
 
         expect(res.json).toHaveBeenCalledWith({name:"this"});
     });
-
-    it("exits gracefully when fetch error", async () => {
-        vi.spyOn(globalThis, "fetch").mockResolvedValue(
-            Response.json({ error: "Service Unavailable" }, { status: 503 })
-        );
-
-        const res = {
-        json: vi.fn(),
-        status: vi.fn().mockReturnThis(),
-        };
-
-        await getRandomMovie({}, res);
-
-        expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({message:"TMDB request failed with status code: 503"});
-    })
 })
